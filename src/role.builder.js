f4e56ec0723f74cff0d4dd018dc9642cf51091af
snakehead007 <snakehead007@protonmail.com>
2020-07-07 03:00:52 +0200

    update

--- src/role.builder.js
+++ src/role.builder.js
@@ -22,7 +22,9 @@ var roleBuilder = {
         }
         else {
             var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+            let harvest = creep.harvest(sources[0]);
+            console.log(harvest);
+            if(harvest == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
             }
         }
--- src/role.harvester.js
+++ src/role.harvester.js
@@ -4,7 +4,9 @@ var roleHarvester = {
     run: function(creep) {
         if(creep.store.getFreeCapacity() > 0) {
             var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+            let harvest = creep.harvest(sources[0]);
+            console.log(harvest);
+            if(harvest == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
             }
         }
