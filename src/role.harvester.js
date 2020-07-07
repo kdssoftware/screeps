8bcb8f9e185fe65895aa991f5c84daec4e21f2d1
snakehead007 <snakehead007@protonmail.com>
2020-07-07 06:53:49 +0200

    update

--- src/role.harvester.js
+++ src/role.harvester.js
@@ -26,8 +26,8 @@ var roleHarvester = {
                 }
             });
             if(targets.length > 0) {
-                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                if(creep.transfer(targets[1], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[1], {visualizePathStyle: {stroke: '#ffffff'}});
                 }
             }
         }
