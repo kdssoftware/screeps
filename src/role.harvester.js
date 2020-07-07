19b8673b357aa1b5cfb358f593c8592be7302ed5
snakehead007 <snakehead007@protonmail.com>
2020-07-07 06:59:26 +0200

    update

--- src/role.harvester.js
+++ src/role.harvester.js
@@ -21,7 +21,7 @@ var roleHarvester = {
                 filter: (structure) => {
                     return (structure.structureType == STRUCTURE_EXTENSION ||
                         structure.structureType == STRUCTURE_SPAWN ||
-                        structure.structureType == STRUCTURE_TOWER) &&
+                        structure.structureType == STRUCTURE_CONTAINER) &&
                         structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
                 }
             });
