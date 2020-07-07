ded51ff61eb7e16fbcf562b0ca70380e0a804c64
snakehead007 <snakehead007@protonmail.com>
2020-07-07 11:59:36 +0200

    update

--- src/main.js
+++ src/main.js
@@ -30,7 +30,7 @@ module.exports.loop = function () {
         }
     }
 
-    spawnSpawning.spawning(60,35,5);
+    spawnSpawning.spawning(35,60,5);
 
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
