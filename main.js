61b203b74ee1f72e6fce4eaebb3f597e6b0bce58
snakehead007 <snakehead007@protonmail.com>
2020-07-07 02:21:35 +0200

    update

---
+++ main.js
@@ -0,0 +1,43 @@
+var roleHarvester = require('role.harvester');
+var roleUpgrader = require('role.upgrader');
+var roleBuilder = require('role.builder');
+var spawnSpawning = require('spawn.spawning');
+module.exports.loop = function () {
+
+    var tower = Game.getObjectById('f670c61705a9700608261c86');
+    if(tower) {
+        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+            filter: (structure) => structure.hits < structure.hitsMax
+        });
+        if(closestDamagedStructure) {
+            tower.repair(closestDamagedStructure);
+        }
+
+        var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        if(closestHostile) {
+            tower.attack(closestHostile);
+        }
+    }
+
+    for(var name in Memory.creeps) {
+        if(!Game.creeps[name]) {
+            delete Memory.creeps[name];
+            console.log('Clearing non-existing creep memory:', name);
+        }
+    }
+
+    spawnSpawning.spawning(50,25,25);
+
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        if(creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
+        }
+        if(creep.memory.role == 'upgrader') {
+            roleUpgrader.run(creep);
+        }
+        if(creep.memory.role == 'builder') {
+            roleBuilder.run(creep);
+        }
+    }
+}
---
+++ spawn.spawning.js
@@ -0,0 +1,76 @@
+
+let doSpawn = (role,body=[WORK,CARRY,MOVE])=>{
+    var newName = role + Game.time;
+    Game.spawns['Spawn1'].spawnCreep(body, newName,
+        {memory: {role: role}});
+};
+
+var spawnSpawing = {
+    spawning: function(harvester,builder,upgrader){
+        let oneHundredPercent = harvester+builder+upgrader;
+        if(oneHundredPercent!= 100){
+            console.log("spawn.spawning arguments not equal to 100%! ",oneHundredPercent);
+        }
+
+        let pop = {total:0,roles:{
+                upgrader:0,builder:0,harvester:0
+            }};
+        for (var i in Game.creeps) {
+            pop.total++;
+            switch(Game.creeps[i].memory.role){
+                case 'upgrader':
+                    pop.roles.upgrader++;
+                    break;
+                case 'builder':
+                    pop.roles.builder++;
+                    break;
+                case 'harvester':
+                    pop.roles.harvester++;
+                    break;
+                default:
+                    console.log("Couldnt find the role "+i.memory.role);
+            }
+        }
+        if(Game.rooms["W23S6"].energyAvailable>=300){
+            let index = 0;
+            for(let i in pop.roles){
+                const percentForCurrentRole = (pop.roles[i]/pop.total)*100.0;
+                switch(i){
+                    case 'upgrader':
+                        if(percentForCurrentRole<upgrader){
+                            doSpawn(i);
+                            return
+                        }
+                        break;
+                    case 'builder':
+                        if(percentForCurrentRole<builder){
+                            doSpawn(i);
+                            return;
+                        }
+                        break;
+                    case 'harvester':
+                        if(percentForCurrentRole<=harvester){
+                            doSpawn(i);
+                            return;
+                        }
+                        break;
+                    default:
+                        console.log('trying to find current role, not found: '+i);
+                        break;
+                }
+                index++;
+            }
+        }else{
+            //console.log('not spawning anything, only '+Game.rooms["W23S6"].energyAvailable+' available');
+        }
+        if(Game.spawns['Spawn1'].spawning) {
+            var spawningCreep = Game.creeps[Game.spawns['Spawn1'].spawning.name];
+            Game.spawns['Spawn1'].room.visual.text(
+                '🛠️' + spawningCreep.memory.role,
+                Game.spawns['Spawn1'].pos.x + 1,
+                Game.spawns['Spawn1'].pos.y,
+                {align: 'left', opacity: 0.8});
+        }
+    }
+};
+module.exports = spawnSpawing;
